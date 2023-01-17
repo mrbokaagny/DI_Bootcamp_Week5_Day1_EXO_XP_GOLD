3cada3a18f80932aca0d869f306f4e7eec12a6c6
mrdevbanks <103212600+mrdevbanks@users.noreply.github.com>
2023-01-17 22:18:12 +0000

    exo 3 fini

--- EXO2/src/app/app.component.ts
+++ EXO2/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor() {}
+
+  urlImage : string = 'https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
 }
